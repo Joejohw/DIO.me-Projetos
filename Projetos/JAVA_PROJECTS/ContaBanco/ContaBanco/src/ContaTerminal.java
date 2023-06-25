0a883b3cb7e873f60c2bd4287105c2acc87070c5
Jonathan-Mirir-PC <jonathan.silva13@outlook.com>
2023-06-25 15:21:36 -0300

    New Project - Terminal Bank

---
+++ Projetos/JAVA_PROJECTS/ContaBanco/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ContaTerminal {
+    public static void main(String[] args) {
+        int numero;
+        float saldo;
+        string nomeCliente;
+        string agencia;
+
+
+        }
+    }
+}
